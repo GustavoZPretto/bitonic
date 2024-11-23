2d3304c57a072f8c7d8cea0c50771b2a85a87c2a
Eduardo Stuani <eduardostuani.cs@gmail.com>
2024-11-23 12:20:14 -0300

    First aumatization changes, need to fix string size

---
+++ README.md
@@ -0,0 +1,2 @@
+LENGHT = comprimento maximo de cada string que é lido no 
+N = numero de strings lidas no arquivo de entrada
--- bitonic_openmp/bitonic.c
+++ bitonic_openmp/bitonic.c
@@ -8,8 +8,10 @@
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
+#include <omp.h> // adicionado
+#include <time.h> // adicionado
 
-#define LENGTH 8
+long int LENGTH; 
 
 FILE *fin, *fout;
 
@@ -25,14 +27,58 @@ unsigned long int powersOfTwo[] =
 #define ASCENDING  1
 #define DESCENDING 0
 
-void openfiles() {
-  fin = fopen("sort.in", "r+");
+/** procedure generate_output_filename()
+ eu que criei isso aqui pra gente automatizar o negocio
+ a gente pega o nome do arquivo de entrada e cria um arquivo de saída com mesmo nome só que .out no final
+ **/
+void generate_output_filename(const char *input_file, char *output_file) {
+  strncpy(output_file, input_file, 255);
+  output_file[255] = '\0';
+  char *dot = strrchr(output_file, '.');
+  if (dot && strcmp(dot, ".in") == 0) {
+    strcpy(dot, ".out");
+  } else {
+    strcat(output_file, ".out");
+  }
+}
+
+// TODO: fix this
+/** procedure determine_string_size()
+ eu que criei isso aqui pra gente automatizar o negocio
+ ao inves de ter um define LENGHT (que diz o tamanho da string do arquivo) como constante ou passar como argumento
+ é mais facil a gente determinar o tamanho da string lendo a segunda linha do arquivo (q contem a primeira string a ser lida)
+ só temos que garantir que os arquivos de entrada estao em formato correto
+ **/
+void determine_string_size() {
+  char buffer[1024];
+  
+  // le a primeira linha fodase
+  if (fgets(buffer, sizeof(buffer), fin) == NULL) {
+    perror("fgets");
+    exit(EXIT_FAILURE);
+  }
+  // le a segunda linha que contem a primeira string
+  if (fgets(buffer, sizeof(buffer), fin) == NULL) {
+    perror("fgets");
+    exit(EXIT_FAILURE);
+  }
+
+  buffer[strcspn(buffer, "\n")] = '\0';
+
+  LENGTH = strlen(buffer);
+  
+  // reposiciona o ponteiro
+  rewind(fin);
+}
+
+void openfiles(const char *input_file, const char *output_file) {
+  printf("input_file: %s\n", input_file);
+  fin = fopen(input_file, "r+");
   if (fin == NULL ) {
     perror("fopen fin");
     exit(EXIT_FAILURE);
   }
-
-  fout = fopen("sort.out", "w");
+  fout = fopen(output_file, "w");
   if (fout == NULL ) {
     perror("fopen fout");
     exit(EXIT_FAILURE);
@@ -99,13 +145,24 @@ void BitonicSort() {
 
 /** the main program **/ 
 int main(int argc, char **argv) {
+  if (argc != 3) {
+    printf("Usage: %s <input_file> <num_threads>\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+  const char *input_file = argv[1];
+  int num_threads = atoi(argv[2]);
+  
+  char output_file[256];
+  generate_output_filename(input_file, output_file);
 
   long int i;
 
-  openfiles();
+  openfiles(input_file, output_file);
+  determine_string_size();
+  // esse trecho aq de cima eu criei tb pra gente testar com diferentes arquivos de entrada e tals
 
   fscanf(fin, "%ld", &N);
-
+  printf("N: %ld\n", N);
   if(N > 1073741824 || powersOfTwo[(int)log2(N)] != N) {
     printf("%ld is not a valid number: power of 2 or less than 1073741824!\n", N);
     exit(EXIT_FAILURE);
@@ -120,7 +177,9 @@ int main(int argc, char **argv) {
   for (i = 0; i < N; i++)
     fscanf(fin, "%s", strings + (i * LENGTH));
 
+  clock_t start = clock();
   BitonicSort();
+  clock_t end = clock();
 
   for (i = 0; i < N; i++)
     fprintf(fout, "%s\n", strings + (i * LENGTH));
@@ -128,6 +187,12 @@ int main(int argc, char **argv) {
   free(strings);
   closefiles();
 
+  // esse trecho aq debaixo adicionei tb
+  double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
+  printf("Number of threads: %d\n", num_threads);
+  printf("Array size: %ld\n", N);
+  printf("Input file: %s\n", input_file);
+  printf("Time: %f seconds\n", time_spent);
   return EXIT_SUCCESS;
 }
 
